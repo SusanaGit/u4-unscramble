53f92c9848e6fdf0126ead5e5389f2518afb42c3
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-10-09 12:41:34 +0200

    add comments

--- app/src/main/java/com/susanafigueroa/u4_unscramble/ui/GameViewModel.kt
+++ app/src/main/java/com/susanafigueroa/u4_unscramble/ui/GameViewModel.kt
@@ -6,6 +6,8 @@ import kotlinx.coroutines.flow.MutableStateFlow
 class GameViewModel : ViewModel() {
 
     // represent the state of the UI
+    // MutableStateFlow: emite y guarda el estado actual y notifica a los observadores cada vez que el estado cambia
+    // GameUiState(): valor inicial del flujo. Es una clase de datos.
     private val _uiState = MutableStateFlow(GameUiState())
 
 }
