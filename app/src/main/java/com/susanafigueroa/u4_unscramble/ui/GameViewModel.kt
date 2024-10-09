166ef047251dafcaf891602789486a4d50444e6b
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-10-09 12:36:14 +0200

    create _uiState, that represents the state of the UI

--- app/src/main/java/com/susanafigueroa/u4_unscramble/ui/GameViewModel.kt
+++ app/src/main/java/com/susanafigueroa/u4_unscramble/ui/GameViewModel.kt
@@ -1,6 +1,11 @@
 package com.susanafigueroa.u4_unscramble.ui
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
 
 class GameViewModel : ViewModel() {
+
+    // represent the state of the UI
+    private val _uiState = MutableStateFlow(GameUiState())
+
 }
